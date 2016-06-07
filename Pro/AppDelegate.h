083fe83ed8d19fcee550455231d7fce2e1739e9f
Manas <mdresswalla@gmail.com>
2016-06-07 14:41:39 +0530

    Initial Commit

---
+++ Pro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pro
+//
+//  Created by Apple on 07/06/16.
+//  Copyright © 2016 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pro
+//
+//  Created by Apple on 07/06/16.
+//  Copyright © 2016 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
